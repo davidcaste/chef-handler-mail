8808c2d25a09c346dda59d74702d654a30b76da5
David Castellanos <dcastellanos@fintonic.com>
2017-04-04 17:35:50 +0200

    Fix from_address

--- lib/chef/handler/mail.rb
+++ lib/chef/handler/mail.rb
@@ -25,7 +25,7 @@ class MailHandler < Chef::Handler
   def initialize(opts = {})
     @options = {
       :to_address => "root",
-      :from_address => "chef-client@#{node.fqdn}",
+      :from_address => options[:from_address] || "chef-client@#{node.fqdn}",
       :template_path => File.join(File.dirname(__FILE__), "mail.erb")
     }
     @options.merge! opts
