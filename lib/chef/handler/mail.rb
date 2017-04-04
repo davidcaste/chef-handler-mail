db39fa96ab4900da11c266f6f81cd1ddf90de967
David Castellanos <dcastellanos@fintonic.com>
2017-04-04 17:45:52 +0200

    Fix from_address

--- lib/chef/handler/mail.rb
+++ lib/chef/handler/mail.rb
@@ -23,11 +23,9 @@ require 'pp'
 class MailHandler < Chef::Handler
   attr_reader :options
   def initialize(opts = {})
-    Chef::Log.debug("#{PP.pp(node)}")
-
     @options = {
       :to_address => "root",
-      :from_address => "chef-client@#{node.fqdn}",
+      :from_address => "chef-client@#{Socket.gethostname}",
       :template_path => File.join(File.dirname(__FILE__), "mail.erb")
     }
     @options.merge! opts
